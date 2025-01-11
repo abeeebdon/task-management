97a7c2fb4bea6ccb5a4a45857e007524eb0ab1d1
abeebdon <abeebdon@gmail.com>
2025-01-11 22:22:52 +0100

    Notification screen : UI desigbn

--- src/components/data.tsx
+++ src/components/data.tsx
@@ -2,86 +2,93 @@ export interface TaskType {
   name: string
   due_date: string
   status: string
+  description: string
   priority: string
+  notification?: boolean
 }
 
 export interface NotificationType {
   name: string
   description: string
+  status: boolean
 }
 
 export const tasks: TaskType[] = [
   {
     name: 'Task 1',
+    description:
+      'Find & Download Free Graphic Resources for Letterhead Vectors, Stock Photos & PSD files. ✓ Free for commercial use ✓ High Quality Images.',
     due_date: '2025-01-07',
     status: 'completed',
     priority: 'high',
+    notification: true,
   },
   {
     name: 'Task 2',
+    description:
+      'Find & Download Free Graphic Resources for Letterhead Vectors, Stock Photos & PSD files. ✓ Free for commercial use ✓ High Quality Images.',
+
     due_date: '2025-05-01',
     status: 'in progress',
     priority: 'low',
+    notification: true,
   },
   {
     name: 'Task 3',
+    description:
+      'Find & Download Free Graphic Resources for Letterhead Vectors, Stock Photos & PSD files. ✓ Free for commercial use ✓ High Quality Images.',
+
     due_date: '2024-12-01',
     status: 'not started',
     priority: 'high',
+    notification: false,
+  },
+  {
+    name: 'Task 4',
+    description:
+      'Find & Download Free Graphic Resources for Letterhead Vectors, Stock Photos & PSD files. ✓ Free for commercial use ✓ High Quality Images.',
+
+    due_date: '2025-01-05',
+    status: 'completed',
+    priority: 'high',
+    notification: true,
+  },
+  {
+    name: 'Task 4',
+    description:
+      'Find & Download Free Graphic Resources for Letterhead Vectors, Stock Photos & PSD files. ✓ Free for commercial use ✓ High Quality Images.',
+
+    due_date: '2025-01-05',
+    status: 'completed',
+    priority: 'high',
+    notification: true,
+  },
+  {
+    name: 'Task 4',
+    description:
+      'Find & Download Free Graphic Resources for Letterhead Vectors, Stock Photos & PSD files. ✓ Free for commercial use ✓ High Quality Images.',
+
+    due_date: '2025-01-05',
+    status: 'completed',
+    priority: 'high',
+    notification: false,
   },
   {
     name: 'Task 4',
     due_date: '2025-01-05',
     status: 'completed',
     priority: 'high',
-  },
-  {
-    name: 'Task 4',
-    due_date: '2025-01-05',
-    status: 'completed',
-    priority: 'high',
-  },
-  {
-    name: 'Task 4',
-    due_date: '2025-01-05',
-    status: 'completed',
-    priority: 'high',
-  },
-  {
-    name: 'Task 4',
-    due_date: '2025-01-05',
-    status: 'completed',
-    priority: 'high',
+    notification: true,
+    description:
+      'Find & Download Free Graphic Resources for Letterhead Vectors, Stock Photos & PSD files. ✓ Free for commercial use ✓ High Quality Images.',
   },
   {
     name: 'Task 5',
     due_date: '2022-01-01',
     status: 'completed',
     priority: 'high',
-  },
-  {
-    name: 'Task 6',
-    due_date: '2025-01-06',
-    status: 'completed',
-    priority: 'high',
-  },
-  {
-    name: 'Task 6',
-    due_date: '2025-01-06',
-    status: 'completed',
-    priority: 'high',
-  },
-  {
-    name: 'Task 6',
-    due_date: '2025-01-06',
-    status: 'completed',
-    priority: 'high',
-  },
-  {
-    name: 'Task 6',
-    due_date: '2025-01-06',
-    status: 'completed',
-    priority: 'high',
+    notification: true,
+    description: '',
   },
 ]
 
@@ -92,9 +99,41 @@ export const notifications: NotificationType[] = [
   {
     name: 'Conversion',
     description: 'Dollar to naira',
+    status: false,
   },
   {
     name: 'Company add a new task',
     description: 'Conversion',
+    status: false,
+  },
+  {
+    name: 'Company add a new task',
+    description: 'Conversion',
+    status: false,
+  },
+  {
+    name: 'Company add a new task',
+    description: 'Conversion',
+    status: false,
+  },
+  {
+    name: 'Company add a new task',
+    description: 'Conversion',
+    status: false,
+  },
+  {
+    name: 'Company add a new task',
+    description: 'Conversion',
+    status: false,
+  },
+  {
+    name: 'Company add a new task',
+    description: 'Conversion',
+    status: false,
+  },
+  {
+    name: 'Company add a new task',
+    description: 'Conversion',
+    status: false,
   },
 ]
--- src/pages/screens/Notifications.tsx
+++ src/pages/screens/Notifications.tsx
@@ -1,33 +1,156 @@
 import { TiTick } from 'react-icons/ti'
-import { notifications } from '../../components/data'
+import { tasks, TaskType } from '../../components/data'
+import { FaFileCsv, FaTimes, FaUser } from 'react-icons/fa'
+import { useEffect, useState } from 'react'
 
 const Notifications = () => {
+  const [activeNotif, setActiveNotif] = useState(0)
+  const [activeNotifData, setActiveNotifData] = useState<TaskType>(
+    {} as TaskType
+  )
+  const [filterNotification, setFilterNotification] = useState<TaskType[]>()
+  useEffect(() => {
+    const filterNotif = tasks.filter((notif) => notif.notification === true)
+    setFilterNotification(filterNotif)
+    if (filterNotif.length > 0) {
+      setActiveNotifData(filterNotif[activeNotif])
+    }
+  }, [activeNotif])
+  const handleNotif = (id: number) => {
+    setActiveNotif(id)
+  }
   return (
-    <section className="flex flex-col sm:flex-row gap-4 items-center">
-      <article className="basis-[40%]">
+    <section className="flex flex-col sm:flex-row gap-4 justify-between">
+      <article className="basis-[45%]">
         <div className="flex items-center justify-between">
           <h3>Latest Notifications</h3>
           <TiTick />
         </div>
-        <div className="space-y-4">
-          {notifications.map((notif, i) => {
+        <article className="space-y-4  mt-5 h-full max-h-[78vh] overflow-auto ">
+          {filterNotification?.map((notif, i) => {
             return (
-              <div key={i} className="bg-white">
-                <div
-                  className={`size-5 flex justify-center items-center rounded-full ${
-                    notif.status ? 'bg-dpurple text-white' : 'bg-gray-200'
-                  }`}
-                >
-                  {notif.status && <TiTick />}
+              <div
+                key={i}
+                className={` bg-white flex justify-between p-4 pb-6 rounded cursor-pointer ${
+                  activeNotif === i
+                    ? 'border-l-2 border-blue-800 bg-purple-200'
+                    : 'border-none'
+                }`}
+                onClick={() => handleNotif(i)}
+              >
+                <article className="basis-[70%] flex gap-4">
+                  <div
+                    className={`size-5 flex justify-center items-center rounded-full ${
+                      notif.status ? 'bg-dpurple text-white' : 'bg-gray-200'
+                    }`}
+                  >
+                    {notif.status && <TiTick />}
+                  </div>
+                  <div>
+                    <h5 className="text-sm">{notif.name}</h5>
+                    <p className="text-xs">{notif.description}</p>
+                  </div>
+                </article>
+                {
+                  // user image comes here
+                }
+                <div className="">
+                  <FaUser />
                 </div>
-                <h5>{notif.name}</h5>
-                <p>{notif.description}</p>
               </div>
             )
           })}
+        </article>
+      </article>
+      <article className="bg-white rounded-2xl mt-5 basis-[45%] h-full p-4">
+        <article className="space-y-4">
+          <div className="flex items-center justify-between ">
+            <h2 className="text-xl font-bold">Company Research</h2>
+            <FaTimes />
+          </div>
+          <section className="space-y-4">
+            <article className="flex items-center gap-10">
+              <div className="flex items-center gap-2 text-xs">
+                <FaUser />
+                <p>Assignee</p>
+              </div>
+              <div className="flex items-center gap-2 border bg-red-200 text-xs p-1 rounded">
+                <FaUser />
+                <p>Me</p>
+              </div>
+            </article>
+            <article className="flex items-center gap-10">
+              <div className="flex items-center gap-2 text-xs">
+                <FaUser />
+                <p>Assignee</p>
+              </div>
+              <div className="flex items-center gap-2 border bg-red-200 text-xs p-1 rounded">
+                <FaUser />
+                <p>Me</p>
+              </div>
+            </article>
+            <article className="flex items-center gap-10">
+              <div className="flex items-center gap-2 text-xs">
+                <FaUser />
+                <p>Assignee</p>
+              </div>
+              <div className="flex items-center gap-2 border bg-red-200 text-xs p-1 rounded">
+                <FaUser />
+                <p>Me</p>
+              </div>
+            </article>
+            <article className="flex items-center gap-10">
+              <div className="flex items-center gap-2 text-xs">
+                <FaUser />
+                <p>Assignee</p>
+              </div>
+              <div className="flex items-center gap-2 border bg-red-200 text-xs p-1 rounded">
+                <FaUser />
+                <p>Me</p>
+              </div>
+            </article>
+          </section>
+        </article>
+
+        {/* Attachment s  */}
+        <section className="mt-4">
+          <h2>Attachments</h2>
+          <p>{activeNotifData?.name}</p>
+          <div className="p-3 bg-red- flex items-center justify-between">
+            <article className="flex items-center gap-2 text-xs">
+              <FaFileCsv />
+              <p>No attachments</p>
+            </article>
+            <article className="flex items-center gap-2 text-xs">
+              <FaFileCsv />
+              <p>No attachments</p>
+            </article>
+          </div>
+        </section>
+        {/* links  */}
+        <section className="mt-4">
+          <h2>Attachments</h2>
+          <div className="p-3 bg-red- flex items-center justify-between">
+            <article className="flex items-center gap-2 text-xs">
+              <FaFileCsv />
+              <p>No attachments</p>
+            </article>
+            <article className="flex items-center gap-2 text-xs">
+              <FaFileCsv />
+              <p>No attachments</p>
+            </article>
+          </div>
+        </section>
+
+        <div className="flex mt-10  gap-4">
+          <button className="bg-white text-blue-400 w-full p-2 rounded mt-4">
+            Archive Task
+          </button>
+          <button className="bg-white text-blue-400 w-full p-2 rounded mt-4">
+            Delete Task
+          </button>
         </div>
       </article>
-      <article></article>
     </section>
   )
 }
